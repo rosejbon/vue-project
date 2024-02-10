950d9c27ac30513a57ac2aceb9b43fc99a691887
Rose Bonner <rose@neontribe.co.uk>
2024-02-10 16:05:02 +0000

    add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+        <button class="button">
+            <slot></slot>
+        </button>
+</template>
+  
+<style scoped>
+.button {
+    border-radius: 20px;
+    background-color: var(--color-red);
+    color: var(--color-white);
+    padding: .66667em 1.5em;
+    text-decoration: none;
+    display: inline-block;
+}
+
+.button:hover,
+.button:active,
+.button:focus-visible {
+    background-color: var(--color-green);
+    color: var(--color-white);
+    border: none;
+    outline: none;
+}
+</style>
