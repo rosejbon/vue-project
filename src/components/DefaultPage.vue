4e068195dceb9ca05dee3ea5c93cbe6e753efdfa
Rose Bonner <rose@neontribe.co.uk>
2024-02-10 19:03:09 +0000

    add default pages

---
+++ src/components/DefaultPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <Box padding="space-xl" background="blue">
+        <h1>Lorem ipsum dolor sit amet</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Nibh ipsum consequat nisl vel. Curabitur vitae nunc sed velit. Porttitor leo a diam sollicitudin
+            tempor id eu. Pretium lectus quam id leo in vitae turpis. Enim nec dui nunc mattis enim ut tellus. Scelerisque
+            fermentum dui faucibus in ornare quam. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras.
+            Auctor neque vitae tempus quam pellentesque. In cursus turpis massa tincidunt dui ut ornare lectus sit. Amet
+            tellus cras adipiscing enim eu turpis egestas pretium. Cum sociis natoque penatibus et magnis dis parturient
+            montes. Gravida in fermentum et sollicitudin ac orci phasellus egestas. At quis risus sed vulputate odio ut enim
+            blandit volutpat.
+        </p>
+        <p>
+            Imperdiet proin fermentum leo vel orci porta. Nunc consequat interdum varius sit amet mattis vulputate. Integer
+            vitae justo eget magna fermentum iaculis eu non diam. In pellentesque massa placerat duis ultricies lacus sed
+            turpis. Et odio pellentesque diam volutpat. Nec tincidunt praesent semper feugiat nibh. Libero id faucibus nisl
+            tincidunt. Id leo in vitae turpis massa sed elementum tempus. Nec tincidunt praesent semper feugiat. Maecenas
+            volutpat blandit aliquam etiam erat velit scelerisque in. Massa eget egestas purus viverra. Neque volutpat ac
+            tincidunt vitae semper quis lectus nulla. Cras sed felis eget velit aliquet sagittis id. Vulputate eu
+            scelerisque felis imperdiet proin fermentum leo vel. A pellentesque sit amet porttitor eget. Amet consectetur
+            adipiscing elit ut aliquam purus. Dui id ornare arcu odio ut. Nec dui nunc mattis enim ut tellus elementum.
+            Faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing.
+        </p>
+    </Box>
+</template>
+
+<script lang="ts" setup>
+import Box from './Box.vue'
+</script>
