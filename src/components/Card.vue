8df0f33cfea6e272757715350e535213805bf20b
Rose Bonner <rose@neontribe.co.uk>
2024-02-10 16:29:54 +0000

    add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+
+import Box from './Box.vue'
+import Link from './Link.vue'
+import Stack from './Stack.vue';
+
+export interface Props {
+    title: string,
+    text: string,
+    callToActionText: string,
+    callToActionLink: string,
+}
+
+defineProps<Props>()
+
+</script>
+
+<template>
+    <Box padding="space-m" background="white">
+        <Stack gap="space-s" :style="`align-items : flex-start`">
+            <h3>{{ title }}</h3>
+            <p>{{ text }}</p>
+            <Link :page="callToActionLink" :text="callToActionText" />
+        </Stack>
+    </Box>
+</template>
