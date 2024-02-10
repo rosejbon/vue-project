b95d08aa9f1e2bc41564df7e431a9eefabbb575b
Rose Bonner <rose@neontribe.co.uk>
2024-02-10 16:01:44 +0000

    add box component

---
+++ src/components/Box.vue
@@ -0,0 +1,36 @@
+<!-- Based off the Box component from Every Layout - https://every-layout.dev/layouts/box/ -->
+
+<template>
+    <div class="box" :style="computedStyles">
+        <slot></slot>
+    </div>
+</template>
+  
+<script lang="ts" setup>
+import { defineProps, computed } from 'vue'
+
+interface Props {
+    padding?: 'space-3xs' | 'space-2xs' | 'space-xs' | 'space-s' | 'space-m' | 'space-l' | 'space-xl' | 'space-2xl' | 'space-3xl',
+    background?: 'white' | 'green' | 'red' | 'off-white' | 'blue'
+}
+
+const props = defineProps<Props>()
+
+const computedStyles = computed(() => {
+    let styles = '';
+    if (props.padding) {
+        styles += `padding: var(--${props.padding}); `;
+    }
+    if (props.background) {
+        styles += `background: var(--color-${props.background}); `;
+    }
+    return styles.trim();
+})
+</script>
+  
+<style scoped>
+.box * {
+    color: inherit;
+}
+</style>
+  
